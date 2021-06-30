5018d9fff4ab8b6073ce9b8acfcf69d56fc8d818
doga <doga.budak@check24.de>
2021-06-30 15:40:48 +0200

    minor changes commited for device change

--- src/pages/destination/destination.js
+++ src/pages/destination/destination.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {connect} from 'react-redux';
 import {Picker} from '@react-native-community/picker';
 
