9e40b8703de23193f58641449e3cfa57c3212b9a
doga budak <dogabudak@dogas-MacBook-Air.local>
2022-01-08 21:54:03 +0000

    Updated many packages and configured the android run

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ import App from './src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+if (!__DEV__) {
+  for (const iterator of Object.keys(global.console)) {
+    global.console[iterator] = () => 0;
+  }
+}
